45f890daafc5bbef59313b037e8d0bcc2d10226e
Cyril Ajieh <cyril.ajieh@cyrils-mbp.attlocal.net>
2017-07-30 03:54:18 -0500

    implemented contacts list view and  delete button. Search contact is in progress

---
+++ src/ListContacts.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import { PropTypes } from 'prop-types';
+
+class ListContacts extends Component  {
+    state ={
+        query:''
+    }
+    updateQuery= (query) => {
+     this.setState({ query : query.trim()})
+    } 
+    render(){
+        return (
+         <div className="list-conatcs"> 
+             { JSON.stringify(this.state)}
+             <div className="list-conatcs-top"> 
+              <input type="text" placeholder="Search Contact" className="search-contacts"
+              value={this.state.query} onChange={(event) => this.updateQuery(event.target.value)} />
+           </div>
+           <ol className='contact-list'>
+            {this.props.contacts.map((contact) => (
+            <li key={contact.id} className='contact-list-item'>
+                <div className='contact-avatar' style={{
+                backgroundImage: `url(${contact.avatarURL})`
+                }}/>
+                <div className='contact-details'>
+                <p>{contact.name}</p>
+                <p>{contact.email}</p>
+                </div>
+                <button onClick={()=> this.props.onRemoveContact(contact)} className='contact-remove'>
+                Remove
+                </button>
+            </li>
+            ))}
+        </ol>
+        </div>
+        )
+    }
+  
+}
+ListContacts.propTypes ={
+    contacts: PropTypes.array.isRequired,
+    onRemoveContact: PropTypes.func.isRequired
+}
+
+export default ListContacts
